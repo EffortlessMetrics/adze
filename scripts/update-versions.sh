040b06deafffa4ce97af6b784f94a1ac0a39abab
Steven Zimmerman <git@effortlesssteven.com>
2025-08-04 06:47:07 -0400

    feat: update all crate versions to 1.0.0

--- scripts/update-versions.sh
+++ scripts/update-versions.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
-# Script to update all crate versions for beta release
+# Script to update all crate versions
 
-VERSION="0.5.0-beta"
+OLD_VERSION="0.5.0-beta"
+VERSION="1.0.0"
 
-echo "Updating all crate versions to $VERSION"
+echo "Updating all crate versions from $OLD_VERSION to $VERSION"
 
 # Update workspace members
-sed -i "s/version = \"0.4.5\"/version = \"$VERSION\"/g" runtime/Cargo.toml
-sed -i "s/version = \"0.4.5\"/version = \"$VERSION\"/g" macro/Cargo.toml
-sed -i "s/version = \"0.4.5\"/version = \"$VERSION\"/g" tool/Cargo.toml
-sed -i "s/version = \"0.4.5\"/version = \"$VERSION\"/g" common/Cargo.toml
-sed -i "s/version = \"0.4.5\"/version = \"$VERSION\"/g" ir/Cargo.toml
-sed -i "s/version = \"0.4.5\"/version = \"$VERSION\"/g" glr-core/Cargo.toml
-sed -i "s/version = \"0.4.5\"/version = \"$VERSION\"/g" tablegen/Cargo.toml
+sed -i "s/version = \"$OLD_VERSION\"/version = \"$VERSION\"/g" runtime/Cargo.toml
+sed -i "s/version = \"$OLD_VERSION\"/version = \"$VERSION\"/g" macro/Cargo.toml
+sed -i "s/version = \"$OLD_VERSION\"/version = \"$VERSION\"/g" tool/Cargo.toml
+sed -i "s/version = \"$OLD_VERSION\"/version = \"$VERSION\"/g" common/Cargo.toml
+sed -i "s/version = \"$OLD_VERSION\"/version = \"$VERSION\"/g" ir/Cargo.toml
+sed -i "s/version = \"$OLD_VERSION\"/version = \"$VERSION\"/g" glr-core/Cargo.toml
+sed -i "s/version = \"$OLD_VERSION\"/version = \"$VERSION\"/g" tablegen/Cargo.toml
 
 # Update internal dependencies
-sed -i "s/rust-sitter-macro = { version = \"0.4.5\"/rust-sitter-macro = { version = \"$VERSION\"/g" runtime/Cargo.toml
-sed -i "s/rust-sitter-common = { version = \"0.4.5\"/rust-sitter-common = { version = \"$VERSION\"/g" macro/Cargo.toml
-sed -i "s/rust-sitter-common = { version = \"0.4.5\"/rust-sitter-common = { version = \"$VERSION\"/g" tool/Cargo.toml
-sed -i "s/rust-sitter = { version = \"0.4.5\"/rust-sitter = { version = \"$VERSION\"/g" example/Cargo.toml
-sed -i "s/rust-sitter-tool = { version = \"0.4.5\"/rust-sitter-tool = { version = \"$VERSION\"/g" example/Cargo.toml
+sed -i "s/rust-sitter-macro = { version = \"$OLD_VERSION\"/rust-sitter-macro = { version = \"$VERSION\"/g" runtime/Cargo.toml
+sed -i "s/rust-sitter-common = { version = \"$OLD_VERSION\"/rust-sitter-common = { version = \"$VERSION\"/g" macro/Cargo.toml
+sed -i "s/rust-sitter-common = { version = \"$OLD_VERSION\"/rust-sitter-common = { version = \"$VERSION\"/g" tool/Cargo.toml
+sed -i "s/rust-sitter-ir = { version = \"$OLD_VERSION\"/rust-sitter-ir = { version = \"$VERSION\"/g" glr-core/Cargo.toml
+sed -i "s/rust-sitter-ir = { version = \"$OLD_VERSION\"/rust-sitter-ir = { version = \"$VERSION\"/g" tablegen/Cargo.toml
+sed -i "s/rust-sitter-glr-core = { version = \"$OLD_VERSION\"/rust-sitter-glr-core = { version = \"$VERSION\"/g" tablegen/Cargo.toml
+sed -i "s/rust-sitter = { version = \"$OLD_VERSION\"/rust-sitter = { version = \"$VERSION\"/g" example/Cargo.toml
+sed -i "s/rust-sitter-tool = { version = \"$OLD_VERSION\"/rust-sitter-tool = { version = \"$VERSION\"/g" example/Cargo.toml
 
 echo "Version update complete!"
