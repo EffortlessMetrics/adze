8b889b91c1eb9863e84db71172d2b402e10dd421
Steven Zimmerman <git@effortlesssteven.com>
2025-08-26 14:25:06 -0400

    Apply remaining clippy formatting fixes from PR #39 merge

--- glr-core/tests/test_json_recovery.rs
+++ glr-core/tests/test_json_recovery.rs
@@ -1,7 +1,7 @@
 //! Test JSON parsing with error recovery to verify our EOF and error stats fixes
 #![allow(clippy::unwrap_or_default, clippy::unnecessary_get_then_check)]
 
-use rust_sitter_glr_core::{Driver, FirstFollowSets, build_lr1_automaton};
+use rust_sitter_glr_core::{build_lr1_automaton, Driver, FirstFollowSets};
 use rust_sitter_ir::{Grammar, ProductionId, Rule, Symbol, SymbolId, Token, TokenPattern};
 
 fn create_json_grammar() -> Grammar {
